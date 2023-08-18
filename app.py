1d3106074edeb6dc769ae76af23e58969f91e35c
Ahmed Okasha <hmdokasha@gmail.com>
2023-08-18 13:56:02 +0300

    Streamlit app
    Beginning the project and launching streamlit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import numpy as np
+import plotly.graph_objects as go
+
+st.set_page_config(page_title="DSP Signal processing",layout="wide")
+st.title("DSP signal")
+st.subheader("This is where we will put the signal:")
+
